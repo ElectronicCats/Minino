c9a884c55c5205e79049797f11228046288e7ed9
Sabas <s@electroniccats.com>
2023-03-02 15:30:25 -0600

    Update example spoof for ESP32-S3

--- examples/spoof/spoof.ino
+++ examples/spoof/spoof.ino
@@ -1,49 +1,67 @@
-#include <ArduinoBLE.h>
-
 /*
   Salvador Mendoza - Metabase Q
   
   Spoof Apple AirTag advertising package
   Aug 10th, 2022
+
+  Andres Sabas - Mod for ESP32-S3 
+  Mar 2, 2023
 */
+#include <ArduinoBLE.h>
+
+BLEService myService("fff0");
+BLEIntCharacteristic myCharacteristic("fff1", BLERead | BLEBroadcast);
 
 uint8_t advertisement[31] = { //Data to be advertise
   0x1E, 0xFF, 0x4C, 0x00, 0x12, 0x19, 0x10, 0x12, 0x12, 0x34, 0x56, 
   0x78, 0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56, 0x78, 0x90, 
   0x12, 0x34, 0x56, 0x78, 0x90, 0x12, 0x34, 0x56, 0x90
-  };                        
+  };    
 
+// Advertising parameters should have a global scope. Do NOT define them in 'setup' or in 'loop'
+const uint8_t manufactData[4] = {0x01, 0x02, 0x03, 0x04};
+const uint8_t serviceData[3] = {0x00, 0x01, 0x02};
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-  
-  // Begin initialization
+
   if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+    Serial.println("failed to initialize BLE!");
     while (1);
   }
 
-  Serial.println("BLE Central scan");
+  myService.addCharacteristic(myCharacteristic);
+  BLE.addService(myService);
 
-  // Start scanning for peripheral
-  BLE.scan();
-}
+  // Build scan response data packet
+  BLEAdvertisingData scanData;
+  // Set parameters for scan response packet
+  //scanData.setLocalName("Test enhanced advertising");
+  // Copy set parameters in the actual scan response packet
+  //BLE.setScanResponseData(scanData);
 
-void spoofDevice(){ //Spoof advertising data
+  // Build advertising data packet
+  BLEAdvertisingData advData;
+
   uint8_t adv[27];
   for (int x = 4; x < 31; x++) {
     adv[x-4] = (byte)advertisement[x];
   }
 
   uint16_t idm = 0x004c; //Apple ID
-  //setAdvertisingData(BLEAdvertisingData& advertisingData)
-
-  BLE.setManufacturerData(idm, adv, sizeof(adv));
-  BLE.advertise();
   
+  // Set parameters for advertising packet
+  advData.setManufacturerData(idm, adv, sizeof(adv));
+  advData.setAdvertisedService(myService);
+  advData.setAdvertisedServiceData(0xfff0, serviceData, sizeof(serviceData));
+  // Copy set parameters in the actual advertising packet
+  BLE.setAdvertisingData(advData);
+
+  BLE.advertise();
+  Serial.println("advertising ...");
 }
 
-void loop(){
-  spoofDevice();
+void loop() {
+  BLE.poll();
 }
