11cf34c030ad1f02bcf5fa40590836a82e3fb086
Deimos <deimoshall@gmail.com>
2023-05-22 18:40:55 -0600

    feat: add a directional buttons class

---
+++ examples/Menu/DirectionalButtons.h
@@ -0,0 +1,23 @@
+#include <ezButton.h>
+
+#include "Arduino.h"
+
+#define DEBOUNCE_DELAY_MS 50
+#define UP_PIN 5
+#define DOWN_PIN 6
+#define RIGHT_PIN 10
+#define LEFT_PIN 9
+#define SELECT_PIN 46
+
+class DirectionalButtons {
+ public:
+  DirectionalButtons();
+  void loop();
+
+ private:
+  ezButton up;
+  ezButton down;
+  ezButton right;
+  ezButton left;
+  ezButton select;
+};
