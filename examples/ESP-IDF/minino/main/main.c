2631f7b73272a5146a179373ee637930615c2809
deimos <deimoshall@gmail.com>
2024-03-25 13:16:30 -0600

    feat: add gps component

--- examples/ESP-IDF/minino/main/main.c
+++ examples/ESP-IDF/minino/main/main.c
@@ -10,6 +10,7 @@
 #include "bluetooth_scanner.h"
 #include "driver/ledc.h"
 #include "leds.h"
+#include "gps.h"
 
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
@@ -71,6 +72,42 @@ void led_off() {
     ESP_ERROR_CHECK(ledc_timer_resume(LEDC_MODE, LEDC_TIMER));
 }
 
+#define TIME_ZONE (+8)   //Beijing Time
+#define YEAR_BASE (2000) //date in GPS starts from 2000
+
+/**
+ * @brief GPS Event Handler
+ *
+ * @param event_handler_arg handler specific arguments
+ * @param event_base event base, here is fixed to ESP_NMEA_EVENT
+ * @param event_id event id
+ * @param event_data event specific arguments
+ */
+static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    gps_t *gps = NULL;
+    switch (event_id) {
+    case GPS_UPDATE:
+        gps = (gps_t *)event_data;
+        /* print information parsed from GPS statements */
+        ESP_LOGI(TAG, "%d/%d/%d %d:%d:%d => \r\n"
+                 "\t\t\t\t\t\tlatitude   = %.05f°N\r\n"
+                 "\t\t\t\t\t\tlongitude = %.05f°E\r\n"
+                 "\t\t\t\t\t\taltitude   = %.02fm\r\n"
+                 "\t\t\t\t\t\tspeed      = %fm/s",
+                 gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
+                 gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
+                 gps->latitude, gps->longitude, gps->altitude, gps->speed);
+        break;
+    case GPS_UNKNOWN:
+        /* print unknown statements */
+        ESP_LOGW(TAG, "Unknown statement:%s", (char *)event_data);
+        break;
+    default:
+        break;
+    }
+}
+
 void app_main(void) {
     leds_init();
     leds_on();
@@ -82,4 +119,11 @@ void app_main(void) {
     leds_off();
     xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
     printf("Hello world!\n");
+
+    /* NMEA parser configuration */
+    nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
+    /* init NMEA parser library */
+    nmea_parser_handle_t nmea_hdl = nmea_parser_init(&config);
+    /* register event handler for NMEA parser library */
+    nmea_parser_add_handler(nmea_hdl, gps_event_handler, NULL);
 }
