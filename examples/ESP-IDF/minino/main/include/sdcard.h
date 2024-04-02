2563150fb5b28f553da1cd4052f966001e5295cc
deimos <deimoshall@gmail.com>
2024-04-02 08:01:48 -0600

    feat: add sd card test

---
+++ examples/ESP-IDF/minino/main/include/sdcard.h
@@ -0,0 +1,7 @@
+#ifndef SDCARD_H
+#define SDCARD_H
+
+void sdcard_init();
+void sdcard_test();
+
+#endif
--- examples/ESP-IDF/minino/main/main.c
+++ examples/ESP-IDF/minino/main/main.c
@@ -5,6 +5,7 @@
 #include "freertos/task.h"
 #include "keyboard.h"
 #include "leds.h"
+#include "sdcard.h"
 #include "thread_cli.h"
 
 static const char* TAG = "main";
@@ -19,11 +20,13 @@ void hello_task(void* pvParameter) {
 void app_main(void) {
     leds_init();
     leds_on();
+    sdcard_test();
     buzzer_init();
     bluetooth_scanner_init();
     thread_cli_init();
     display_init();
-    keyboard_init();  // Init the keyboard after the display to avoid skipping the logo
+    keyboard_init();  // Init the keyboard after the display to avoid skipping
+                      // the logo
     leds_off();
     xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
     printf("Hello world!\n");
