790e746eb82d6595e949c9835d4b2391c9855789
deimos <deimoshall@gmail.com>
2024-07-24 13:04:48 -0600

    fix: do not overwrite csv file

--- examples/minino/main/modules/gps/wardriving/wardriving_module.c
+++ examples/minino/main/modules/gps/wardriving/wardriving_module.c
@@ -23,27 +23,16 @@
 #define BODY           "3"
 #define SUB_BODY       "0"
 
-#define MAC_ADDRESS_FORMAT "%02x:%02x:%02x:%02x:%02x:%02x"
-#define CSV_MAC_FORMAT     "%s"
-#define CSV_SSID_FORMAT    "%s"
-#define CSV_AUTH_MODE      "%s"
-#define CSV_FIRST_SEEN     "%s"
-#define CSV_CHANNEL        "%d"
-#define CSV_FREQUENCY      "%u"
-#define CSV_RSSI           "%d"
-#define CSV_CURRENT_LAT    "%f"
-#define CSV_CURRENT_LON    "%f"
-#define CSV_ALTITUDE       "%f"
-#define CSV_ACCURACY       "%f"
-#define CSV_RCOIS          "%s"
-#define CSV_MFGRID         "%s"
-#define CSV_TYPE           "%s"
-
+#define MAC_ADDRESS_FORMAT     "%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAX_CSV_LINES          100
+#define CSV_LINE_SIZE          200  // Got it from real time tests
+#define CSV_FILE_SIZE          CSV_LINE_SIZE* MAX_CSV_LINES
 #define WIFI_SCAN_INTERVAL_SEC 5
 
 const char* TAG = "wardriving";
 TaskHandle_t wardriving_module_scan_task_handle = NULL;
 gps_t* gps = NULL;
+bool gps_valid = false;
 
 const char* csv_header = FORMAT_VERSION
     ",appRelease=" APP_VERSION ",model=" MODEL ",release=" RELEASE
@@ -213,6 +202,10 @@ uint16_t get_frequency(uint8_t primary) {
 
 void wardriving_module_scan_task(void* pvParameters) {
   wifi_driver_init_sta();
+  uint16_t csv_lines = 2;  // Two header lines
+  char* csv_file = malloc(CSV_FILE_SIZE);
+  sprintf(csv_file, "%s\n", csv_header);  // Append header to csv file
+
   while (true) {
     wifi_scanner_module_scan();
     vTaskDelay(5000 / portTICK_PERIOD_MS);
@@ -224,18 +217,15 @@ void wardriving_module_scan_task(void* pvParameters) {
     ESP_LOGI(TAG, "Satellites in use: %d, valid: %s", gps->sats_in_use,
              gps->valid ? "true" : "false");
     if (gps->sats_in_use == 0) {
-      continue;
+      // continue;
     }
 
     wifi_scanner_ap_records_t* ap_records = wifi_scanner_get_ap_records();
-    char* csv_line = malloc(1024);
-    char* csv_file = malloc(1024);
-
-    // Append header to csv file
-    sprintf(csv_file, "%s\n", csv_header);
+    char* csv_line = malloc(CSV_LINE_SIZE);
 
     // Append records to csv file
     for (int i = 0; i < ap_records->count; i++) {
+      csv_lines++;
       char* mac_address_str = get_mac_address(ap_records->records[i].bssid);
       char* auth_mode_str = get_auth_mode(ap_records->records[i].authmode);
       char* full_date_time = get_full_date_time(gps);
@@ -274,8 +264,10 @@ void wardriving_module_scan_task(void* pvParameters) {
       free(full_date_time);
 
       ESP_LOGI(TAG, "CSV Line: %s", csv_line);
+      ESP_LOGI(TAG, "Line size %d bytes", strlen(csv_line));
       strcat(csv_file, csv_line);
     }
+    ESP_LOGW(TAG, "File size %d bytes, lines: %u", strlen(csv_file), csv_lines);
     sd_card_write_file(FILE_NAME, csv_file);
     sd_card_read_file(FILE_NAME);
   }
@@ -290,13 +282,8 @@ void wardriving_module_scan_task(void* pvParameters) {
  * ussually every second.
  */
 void wardriving_gps_event_handler_cb(void* event_data) {
-  static uint32_t counter = 0;
-  counter++;
+  // Update gps data to use it in the scan task
   gps = gps_module_get_instance(event_data);
-
-  ESP_LOGI(TAG, "Date: %d/%d/%d Time: %d:%d:%d", gps->date.year,
-           gps->date.month, gps->date.day, gps->tim.hour, gps->tim.minute,
-           gps->tim.second);
 }
 
 void wardriving_begin() {
@@ -313,41 +300,41 @@ void wardriving_begin() {
   gps_module_start_read();
   return;
 
-  wifi_scanner_ap_records_t* ap_records = wifi_scanner_get_ap_records();
+  // wifi_scanner_ap_records_t* ap_records = wifi_scanner_get_ap_records();
 
-  char* csv_line = malloc(1024);
-  char* csv_file = malloc(1024);
+  // char* csv_line = malloc(1024);
+  // char* csv_file = malloc(1024);
 
-  // Append header to csv file
-  sprintf(csv_file, "%s\n", csv_header);
+  // // Append header to csv file
+  // sprintf(csv_file, "%s\n", csv_header);
 
-  // Append records to csv file
-  for (int i = 0; i < ap_records->count; i++) {
-    sprintf(csv_line, "%s,%s,%s,%s,%d,%u,%d\n",
-            get_mac_address(ap_records->records[i].bssid),
-            ap_records->records[i].ssid,
-            get_auth_mode(ap_records->records[i].authmode),
-            "2021-09-01T00:00:00Z", ap_records->records[i].primary,
-            get_frequency(ap_records->records[i].primary),
-            ap_records->records[i].rssi);
+  // // Append records to csv file
+  // for (int i = 0; i < ap_records->count; i++) {
+  //   sprintf(csv_line, "%s,%s,%s,%s,%d,%u,%d\n",
+  //           get_mac_address(ap_records->records[i].bssid),
+  //           ap_records->records[i].ssid,
+  //           get_auth_mode(ap_records->records[i].authmode),
+  //           "2021-09-01T00:00:00Z", ap_records->records[i].primary,
+  //           get_frequency(ap_records->records[i].primary),
+  //           ap_records->records[i].rssi);
 
-    ESP_LOGI(TAG, "CSV Line: %s", csv_line);
-    strcat(csv_file, csv_line);
-  }
-  sd_card_write_file(FILE_NAME, csv_file);
-  free(csv_line);
-  free(csv_file);
+  //   ESP_LOGI(TAG, "CSV Line: %s", csv_line);
+  //   strcat(csv_file, csv_line);
+  // }
+  // sd_card_write_file(FILE_NAME, csv_file);
+  // free(csv_line);
+  // free(csv_file);
 
-  for (int i = 0; i < ap_records->count; i++) {
-    ESP_LOGI(TAG, "SSID \t\t%s", ap_records->records[i].ssid);
-    ESP_LOGI(TAG, "RSSI \t\t%d", ap_records->records[i].rssi);
-    print_auth_mode(ap_records->records[i].authmode);
-    if (ap_records->records[i].authmode != WIFI_AUTH_WEP) {
-      print_cipher_type(ap_records->records[i].pairwise_cipher,
-                        ap_records->records[i].group_cipher);
-    }
-    ESP_LOGI(TAG, "Channel \t\t%d", ap_records->records[i].primary);
-  }
+  // for (int i = 0; i < ap_records->count; i++) {
+  //   ESP_LOGI(TAG, "SSID \t\t%s", ap_records->records[i].ssid);
+  //   ESP_LOGI(TAG, "RSSI \t\t%d", ap_records->records[i].rssi);
+  //   print_auth_mode(ap_records->records[i].authmode);
+  //   if (ap_records->records[i].authmode != WIFI_AUTH_WEP) {
+  //     print_cipher_type(ap_records->records[i].pairwise_cipher,
+  //                       ap_records->records[i].group_cipher);
+  //   }
+  //   ESP_LOGI(TAG, "Channel \t\t%d", ap_records->records[i].primary);
+  // }
 
   sd_card_read_file(FILE_NAME);
   sd_card_unmount();
