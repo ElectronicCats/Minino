3c1bf6d2c5f0a3beabee16198ccd99fda4c762c1
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2024-07-25 15:51:59 -0600

    feat: Add error wifi handler

--- examples/minino/components/wifi_controller/wifi_controller.c
+++ examples/minino/components/wifi_controller/wifi_controller.c
@@ -84,7 +84,14 @@ void wifi_driver_init_apsta(void) {
 
 void wifi_driver_init_sta(void) {
   ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_err_t err = esp_event_loop_create_default();
+  if (err == ESP_ERR_INVALID_STATE) {
+    ESP_LOGI(TAG_WIFI_DRIVER, "Event loop already created");
+  } else if (err != ESP_OK) {
+    ESP_LOGE(TAG_WIFI_DRIVER, "Error creating event loop: %s",
+             esp_err_to_name(err));
+    esp_restart();
+  }
   esp_netif_t* sta_netif = esp_netif_create_default_wifi_sta();
   assert(sta_netif);
 
@@ -97,7 +104,14 @@ void wifi_driver_init_sta(void) {
 }
 
 void wifi_driver_init_null(void) {
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_err_t err = esp_event_loop_create_default();
+  if (err == ESP_ERR_INVALID_STATE) {
+    ESP_LOGI(TAG_WIFI_DRIVER, "Event loop already created");
+  } else if (err != ESP_OK) {
+    ESP_LOGE(TAG_WIFI_DRIVER, "Error creating event loop: %s",
+             esp_err_to_name(err));
+    esp_restart();
+  }
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
