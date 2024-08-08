b7a3d69efab6ad40106d30c905031bf5b2edf158
Roberto Arellano <bertoarellanosegovi@gmail.com>
2024-08-08 16:53:26 -0600

    fix: infinite loop caused by optimization

--- firmware/main/modules/modals/modals_module.h
+++ firmware/main/modules/modals/modals_module.h
@@ -7,10 +7,10 @@ typedef enum { CANCELED_OPTION = -1, NO_OPTION, YES_OPTION } yes_no_options_t;
 
 typedef struct {
   int8_t selected_option;
-  bool consumed;
   uint8_t options_count;
   char** options;
   char* banner;
+  volatile bool consumed;
 } modal_get_user_selection_t;
 
 int8_t modals_module_get_user_selection(char** options, char* banner);
