f8a3c5b8e802fd69585b42bc5da57a7314b26a25
Roberto Arellano <bertoarellanosegovi@gmail.com>
2024-08-08 17:01:49 -0600

    fix: infinite loop in keyboard modal

--- firmware/main/modules/modals/keyboard/keyboard_modal.h
+++ firmware/main/modules/modals/keyboard/keyboard_modal.h
@@ -5,13 +5,13 @@
 
 typedef struct {
   char* old_text;
+  char* new_text;
   char* banner;
   size_t text_length;
   uint8_t current_char;
-  bool shift;
+  volatile int8_t consumed;
   bool caps;
-  int8_t consumed;
-  char* new_text;
+  bool shift;
 } keyboard_modal_ctx_t;
 
 char* keyboard_module_write(char* text, char* banner);
