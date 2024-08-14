4e250f26e8b43fab3817d566817caca5c15e16d6
Roberto Arellano <bertoarellanosegovi@gmail.com>
2024-08-14 10:04:24 -0600

    feat: new menus navigation system

--- firmware/main/modules/menus_module/menus_module.c
+++ firmware/main/modules/menus_module/menus_module.c
@@ -2,6 +2,8 @@
 
 #include <string.h>
 
+#include "keyboard_module.h"
+#include "menu_screens_modules.h"
 #include "menus_screens.h"
 #include "oled_screen.h"
 
@@ -10,6 +12,7 @@ menus_manager_t* menus_ctx;
 static void update_menus() {
   if (menus_ctx->submenus_idx != NULL) {
     free(menus_ctx->submenus_idx);
+    menus_ctx->submenus_idx = NULL;
   }
   menus_ctx->submenus_idx = NULL;
   menus_ctx->submenus_count = 0;
@@ -37,6 +40,59 @@ static void refresh_menus() {
   update_menus();
   display_menus();
 }
+static void navigation_up() {
+  menus_ctx->selected_menu = menus_ctx->selected_menu == 0
+                                 ? menus_ctx->submenus_count - 1
+                                 : menus_ctx->selected_menu - 1;
+  display_menus();
+}
+static void navigation_down() {
+  menus_ctx->selected_menu =
+      ++menus_ctx->selected_menu < menus_ctx->submenus_count
+          ? menus_ctx->selected_menu
+          : 0;
+  display_menus();
+}
+static void navigation_enter() {
+  if (!menus_ctx->submenus_count) {
+    return;
+  }
+  menus_ctx->current_menu =
+      menus[*menus_ctx->submenus_idx[menus_ctx->selected_menu]].menu_idx;
+  menus_ctx->selected_menu = 0;
+  refresh_menus();
+}
+
+static void navigation_exit() {
+  if (menus_ctx->current_menu == MENU_MAIN_2) {
+    return;
+  }
+  menus_ctx->current_menu = menus[menus_ctx->current_menu].parent_idx;
+  menus_ctx->selected_menu = 0;
+  refresh_menus();
+}
+
+static void menus_input_cb(uint8_t button_name, uint8_t button_event) {
+  if (button_event != BUTTON_PRESS_DOWN) {
+    return;
+  }
+  switch (button_name) {
+    case BUTTON_LEFT:
+      navigation_exit();
+      break;
+    case BUTTON_RIGHT:
+      navigation_enter();
+      break;
+    case BUTTON_UP:
+      navigation_up();
+      break;
+    case BUTTON_DOWN:
+      navigation_down();
+      break;
+    default:
+      break;
+  }
+}
 
 void menus_module_begin() {
   menus_ctx = malloc(sizeof(menus_manager_t));
@@ -44,5 +100,6 @@ void menus_module_begin() {
   menus_ctx->menus_count = sizeof(menus) / sizeof(menu_t);
   printf("Menus Count: %d\n", menus_ctx->menus_count);
   oled_screen_begin();
+  menu_screens_set_app_state(true, menus_input_cb);
   refresh_menus();
 }
