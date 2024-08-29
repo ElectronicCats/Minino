23123a4892dae5b5e8dcd5c72730c0a23457b087
Roberto Arellano <bertoarellanosegovi@gmail.com>
2024-08-29 16:21:12 -0600

    feat: thread broadcast screen refresh improved

--- firmware/main/modules/open_thread/open_thread_screens_module.c
+++ firmware/main/modules/open_thread/open_thread_screens_module.c
@@ -2,6 +2,7 @@
 #include "oled_screen.h"
 
 void open_thread_screens_display_broadcast_mode(uint8_t ch) {
+  oled_screen_clear();
   oled_screen_display_text(" BroadCast Mode ", 0, 0, OLED_DISPLAY_NORMAL);
   char* str = (char*) malloc(18);
   sprintf(str, "  Channel %d    ", ch);
@@ -15,7 +16,7 @@ void open_thread_screens_show_new_message(char* msg) {
   char* str = (char*) malloc(18);
   sprintf(str, "%s", msg);
   oled_screen_clear_line(0, 4, OLED_DISPLAY_NORMAL);
-  oled_screen_display_text(str, 0, 4, OLED_DISPLAY_NORMAL);
+  oled_screen_display_text_center(str, 4, OLED_DISPLAY_NORMAL);
   free(str);
 }
 
