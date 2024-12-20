c281b415197f063595cf1d8c4cd32c884bdd5373
deimos <deimoshall@gmail.com>
2024-12-20 10:35:06 -0600

    feat: replace get_messages by uart_bridge cmd

--- firmware/main/modules/cmd_control/cmd_uart_bridge.c
+++ firmware/main/modules/cmd_control/cmd_uart_bridge.c
@@ -10,7 +10,7 @@
 
 static const char* TAG = "cmd_uart_bridge";
 
-static bool run_get_messages_task = false;
+static bool run_uart_bridge_task = false;
 
 static struct {
   struct arg_str* message;
@@ -21,16 +21,16 @@ static struct {
   struct arg_str* buffer_size;
   struct arg_int* timeout_ms;
   struct arg_end* end;
-} get_messages_args;
+} uart_bridge_args;
 
-// Structure to hold parameters for get_messages_task
+// Structure to hold parameters for uart_bridge_task
 typedef struct {
   const char* buffer_size;
   int timeout_ms;
-} get_messages_params_t;
+} uart_bridge_params_t;
 
 void ctrl_c_callback() {
-  run_get_messages_task = false;
+  run_uart_bridge_task = false;
 }
 
 static void send_message(int argc, char** argv) {
@@ -45,14 +45,14 @@ static void send_message(int argc, char** argv) {
   uart_bridge_write(message, strlen(message));
 }
 
-static void get_messages_task(void* args) {
-  if (run_get_messages_task) {
-    ESP_LOGI(TAG, "get_messages_task already running");
+static void uart_bridge_task(void* args) {
+  if (run_uart_bridge_task) {
+    ESP_LOGI(TAG, "uart_bridge_task already running");
     return;
   }
 
-  run_get_messages_task = true;
-  get_messages_params_t* params = (get_messages_params_t*) args;
+  run_uart_bridge_task = true;
+  uart_bridge_params_t* params = (uart_bridge_params_t*) args;
   const char* buffer_size = params->buffer_size;
   const int timeout_ms = params->timeout_ms;
 
@@ -69,36 +69,36 @@ static void get_messages_task(void* args) {
       memset(buffer, 0, sizeof(buffer));
     }
 
-    if (!run_get_messages_task) {
+    if (!run_uart_bridge_task) {
       break;
     }
   }
   free(params);
-  ESP_LOGI(TAG, "get_messages_task ended :)");
+  ESP_LOGI(TAG, "uart_bridge_task ended :)");
   vTaskDelete(NULL);
 }
 
-static void get_messages(int argc, char** argv) {
-  int nerrors = arg_parse(argc, argv, (void**) &get_messages_args);
+static void uart_bridge(int argc, char** argv) {
+  int nerrors = arg_parse(argc, argv, (void**) &uart_bridge_args);
   if (nerrors != 0) {
-    arg_print_errors(stderr, get_messages_args.end, argv[0]);
+    arg_print_errors(stderr, uart_bridge_args.end, argv[0]);
     return;
   }
 
-  assert(get_messages_args.buffer_size->count == 1);
-  assert(get_messages_args.timeout_ms->count == 1);
-  const char* buffer_size = get_messages_args.buffer_size->sval[0];
-  const int timeout_ms = *get_messages_args.timeout_ms->ival;
+  assert(uart_bridge_args.buffer_size->count == 1);
+  assert(uart_bridge_args.timeout_ms->count == 1);
+  const char* buffer_size = uart_bridge_args.buffer_size->sval[0];
+  const int timeout_ms = *uart_bridge_args.timeout_ms->ival;
 
-  get_messages_params_t* params = malloc(sizeof(get_messages_params_t));
+  uart_bridge_params_t* params = malloc(sizeof(uart_bridge_params_t));
   params->buffer_size = buffer_size;
   params->timeout_ms = timeout_ms;
 
   cat_console_register_ctrl_c_handler(&ctrl_c_callback);
-  ESP_LOGI(TAG, "Starting get_messages_task");
-  xTaskCreate(get_messages_task, "get_messages_task", 4096, (void*) params, 15,
+  ESP_LOGI(TAG, "Starting uart_bridge_task");
+  xTaskCreate(uart_bridge_task, "uart_bridge_task", 4096, (void*) params, 15,
               NULL);
-  ESP_LOGI(TAG, "get_messages_task started");
+  ESP_LOGI(TAG, "uart_bridge_task started");
 }
 
 void register_uart_bridge_commands() {
@@ -119,24 +119,24 @@ void register_uart_bridge_commands() {
       .func = &send_message,
       .argtable = &message_args};
 
-  get_messages_args.buffer_size =
+  uart_bridge_args.buffer_size =
       arg_str1(NULL, NULL, "<buffer_size>",
                "Size in bytes of the buffer to read data into");
-  get_messages_args.timeout_ms =
+  uart_bridge_args.timeout_ms =
       arg_int1(NULL, NULL, "<timeout_ms>",
                "Timeout in milliseconds for reading data\n\n"
-               "\tExample: get_messages 128 1000");
-  get_messages_args.end = arg_end(2);
+               "\tExample: uart_bridge 128 1000");
+  uart_bridge_args.end = arg_end(2);
 
-  esp_console_cmd_t uart_bridge_get_messages_cmd = {
-      .command = "get_messages",
+  esp_console_cmd_t uart_bridge_uart_bridge_cmd = {
+      .command = "uart_bridge",
       .help =
           "Get messages from external UART RXD pin on the MININO.\n"
           "Press Ctrl+C to stop reading messages.",
       .hint = NULL,
-      .func = &get_messages,
-      .argtable = &get_messages_args};
+      .func = &uart_bridge,
+      .argtable = &uart_bridge_args};
 
   ESP_ERROR_CHECK(esp_console_cmd_register(&uart_bridge_print_cmd));
-  ESP_ERROR_CHECK(esp_console_cmd_register(&uart_bridge_get_messages_cmd));
+  ESP_ERROR_CHECK(esp_console_cmd_register(&uart_bridge_uart_bridge_cmd));
 }
