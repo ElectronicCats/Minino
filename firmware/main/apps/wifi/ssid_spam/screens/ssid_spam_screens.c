9b76775d7b8cd015b3d60a9b199a7aa4bb8be54f
Roberto Arellano <bertoarellanosegovi@gmail.com>
2025-02-04 17:31:41 -0600

    refactor: spamming bitmap supported on 128x32

--- firmware/main/apps/wifi/ssid_spam/screens/ssid_spam_screens.c
+++ firmware/main/apps/wifi/ssid_spam/screens/ssid_spam_screens.c
@@ -5,13 +5,17 @@
 #include "oled_screen.h"
 
 void ssid_spam_animation() {
+  uint8_t y = 0;
+#ifdef CONFIG_RESOLUTION_128X64
+  oled_screen_display_text_center("Spamming", 0, OLED_DISPLAY_NORMAL);
+  y = 16;
+#endif
   static uint8_t idx = 0;
-  oled_screen_display_bitmap(punch_animation[idx], 48, 16, 32, 32,
+  oled_screen_display_bitmap(punch_animation[idx], 48, y, 32, 32,
                              OLED_DISPLAY_NORMAL);
   idx = ++idx > (2 - 1) ? 0 : idx;
 }
 
 void ssid_spam_screens_running() {
   oled_screen_clear();
-  oled_screen_display_text_center("Spamming", 0, OLED_DISPLAY_NORMAL);
 }
