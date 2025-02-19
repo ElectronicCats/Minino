771681eb73450dc8fa94b66a78eb04f112a929a1
Roberto Arellano <bertoarellanosegovi@gmail.com>
2025-02-19 11:23:44 -0600

    feat: release file names updated & files ziped

--- firmware/get_builds.sh
+++ firmware/get_builds.sh
@@ -3,7 +3,7 @@ PROJECT_NAME="minino"
 CONFIG_FILE="sdkconfig.version"
 
 if [ -z "$1" ]; then
-  echo "Version parameter is missing. Example: ./get_builds.sh 1.1.7.0"
+  echo "Version parameter is missing. Example: ./get_builds.sh v1.1.7.0"
   exit 1
 fi
 PROJECT_VERSION="$1"
@@ -11,10 +11,10 @@ sed -i "s/^CONFIG_PROJECT_VERSION=.*/CONFIG_PROJECT_VERSION=\"$PROJECT_VERSION\"
 
 
 MININO_BUILD_DIR="build-minino"
-BSIDES_BUILD_DIR="build-bSides"
-DRAGONJAR_BUILD_DIR="build-dragonJar"
-EKOPARTY_BUILD_DIR="build-ekoParty"
-BUGCON_BUILD_DIR="build-bugCon"
+BSIDES_BUILD_DIR="build-bsides"
+DRAGONJAR_BUILD_DIR="build-dragonjar"
+EKOPARTY_BUILD_DIR="build-ekoparty"
+BUGCON_BUILD_DIR="build-bugcon"
 
 FINAL_BUILD_DIR="minino-release-$PROJECT_VERSION"
 
@@ -26,11 +26,19 @@ if [ -d "$FINAL_BUILD_DIR" ]; then
 fi
 
 mkdir -p $FINAL_BUILD_DIR
-cp $MININO_BUILD_DIR/mininoBin.bin $FINAL_BUILD_DIR
-cp $BSIDES_BUILD_DIR/bSidesBin.bin $FINAL_BUILD_DIR
-cp $DRAGONJAR_BUILD_DIR/dragonJarBin.bin $FINAL_BUILD_DIR
-cp $EKOPARTY_BUILD_DIR/ekoPartyBin.bin $FINAL_BUILD_DIR
-cp $BUGCON_BUILD_DIR/bugConBin.bin $FINAL_BUILD_DIR
+cp $MININO_BUILD_DIR/mininoBin.bin $FINAL_BUILD_DIR/minino_$PROJECT_VERSION.bin
+cp $BSIDES_BUILD_DIR/bsidesBin.bin $FINAL_BUILD_DIR/bsides_$PROJECT_VERSION.bin
+cp $DRAGONJAR_BUILD_DIR/dragonjarBin.bin $FINAL_BUILD_DIR/dragonjar_$PROJECT_VERSION.bin
+cp $EKOPARTY_BUILD_DIR/ekopartyBin.bin $FINAL_BUILD_DIR/ekoparty_$PROJECT_VERSION.bin
+cp $BUGCON_BUILD_DIR/bugconBin.bin $FINAL_BUILD_DIR/bugcon_$PROJECT_VERSION.bin
+
+zip $FINAL_BUILD_DIR/minino_$PROJECT_VERSION.zip -j $FINAL_BUILD_DIR/minino_$PROJECT_VERSION.bin
+zip $FINAL_BUILD_DIR/bsides_$PROJECT_VERSION.zip -j $FINAL_BUILD_DIR/bsides_$PROJECT_VERSION.bin
+zip $FINAL_BUILD_DIR/dragonjar_$PROJECT_VERSION.zip -j $FINAL_BUILD_DIR/dragonjar_$PROJECT_VERSION.bin
+zip $FINAL_BUILD_DIR/ekoparty_$PROJECT_VERSION.zip -j $FINAL_BUILD_DIR/ekoparty_$PROJECT_VERSION.bin
+zip $FINAL_BUILD_DIR/bugcon_$PROJECT_VERSION.zip -j $FINAL_BUILD_DIR/bugcon_$PROJECT_VERSION.bin
+
+rm $FINAL_BUILD_DIR/*.bin
 
 make clean_builds
 
