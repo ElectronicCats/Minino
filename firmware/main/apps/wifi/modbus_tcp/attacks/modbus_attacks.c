6f02555ac81df32c376a25ddd74b911ec11fe9a1
Roberto Arellano <bertoarellanosegovi@gmail.com>
2025-04-14 16:08:12 -0600

    feat: validate connection in writer

--- firmware/main/apps/wifi/modbus_tcp/attacks/modbus_attacks.c
+++ firmware/main/apps/wifi/modbus_tcp/attacks/modbus_attacks.c
@@ -1,18 +1,24 @@
 #include "modbus_attacks.h"
 
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "modbus_engine.h"
 
+#define TAG "Modbus_attacks"
+
 static volatile bool attacking = false;
 
 static void writer_task() {
   attacking = true;
-  modbus_engine_connect();
-  while (attacking) {
-    modbus_engine_send_request();
-    vTaskDelay(pdMS_TO_TICKS(100));
+  if (modbus_engine_connect() < 0) {
+    ESP_LOGE(TAG, "Connection Failed");
+  } else {
+    while (attacking) {
+      modbus_engine_send_request();
+      vTaskDelay(pdMS_TO_TICKS(100));
+    }
   }
   modbus_engine_disconnect();
   vTaskDelete(NULL);
