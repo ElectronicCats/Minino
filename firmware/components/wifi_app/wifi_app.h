0b792e097208d27f3f36e639fda890cce3b848d1
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-06-27 10:41:18 -0600

    fix(captive): Remove config from wifi_ap

--- firmware/components/wifi_app/wifi_app.h
+++ firmware/components/wifi_app/wifi_app.h
@@ -2,15 +2,15 @@
 
 #include "esp_netif.h"
 
-#define WIFI_AP_SSID            CONFIG_WIFI_AP_NAME      // MAX 32
-#define WIFI_AP_PASSWORD        CONFIG_WIFI_AP_PASSWORD  // MAX 64
+#define WIFI_AP_SSID            "MININO_AP"  // MAX 32
+#define WIFI_AP_PASSWORD        "Cats1234"   // MAX 64
 #define WIFI_AP_CHANNEL         1
 #define WIFI_AP_SSID_HIDDEN     0
 #define WIFI_AP_MAX_CONNECTIONS 5
 #define WIFI_AP_BEACON_INTERVAL 100
-#define WIFI_AP_IP              CONFIG_WIFI_AP_IP
-#define WIFI_AP_GATEWAY         CONFIG_WIFI_AP_GATEWAY
-#define WIFI_AP_NETMASK         CONFIG_WIFI_AP_NETMASK
+#define WIFI_AP_IP              "192.168.0.1"
+#define WIFI_AP_GATEWAY         "192.168.0.1"
+#define WIFI_AP_NETMASK         "255.255.255.0"
 #define WIFI_AP_BANDWIDTH       WIFI_BW_HT20
 #define WIFI_STA_POWER_SAVE     WIFI_PS_NONE
 #define WIFI_AUTH_MODE          WIFI_AUTH_WPA2_PSK
