fcb5078b6a1ff76d6dcab433affdb3cccacd5cec
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-07-19 09:28:38 -0600

    feat: Adds captive portal redirect functionality
    Adds the functionality to redirect users to a specific HTML page after connecting to the captive portal.
    
    This includes:
    - Adding a new "redirect" option to the captive portal menu.
    - Implementing a new handler for the "/redirect" URI.
    - Adding functions to read and manage redirect HTML files from the SD card.
    - Including a default redirect.html file.

--- firmware/main/apps/wifi/captive/captive_module.h
+++ firmware/main/apps/wifi/captive/captive_module.h
@@ -3,15 +3,18 @@
 
 #include "general_filedir.h"
 
-#define CAPTIVE_PORTAL_PATH_NAME       "portals"
-#define CAPTIVE_PORTAL_DEFAULT_NAME    "root.html"
-#define CAPTIVE_PORTAL_LIMIT_PORTALS   20
-#define CAPTIVE_PORTAL_MAX_DEFAULT_LEN 24
-#define CAPTIVE_PORTAL_MODE_FS_KEY     "cpmode"
-#define CAPTIVE_PORTAL_FS_KEY          "cpportal"
-#define CAPTIVE_PORTAL_PREF_FS_KEY     "cpprefs"
-#define CAPTIVE_PORTAL_CHANNEL         "cpchan"
-#define CAPTIVE_PORTAL_NET_NAME        "WIFI_AP_DEF"
+#define CAPTIVE_PORTAL_PATH_NAME             "portals"
+#define CAPTIVE_PORTAL_REDIRECT_PATH_NAME    "redirect"
+#define CAPTIVE_PORTAL_DEFAULT_NAME          "root.html"
+#define CAPTIVE_PORTAL_REDIRECT_DEFAULT_NAME "redirect.html"
+#define CAPTIVE_PORTAL_LIMIT_PORTALS         20
+#define CAPTIVE_PORTAL_MAX_DEFAULT_LEN       24
+#define CAPTIVE_PORTAL_MODE_FS_KEY           "cpmode"
+#define CAPTIVE_PORTAL_FS_KEY                "cpportal"
+#define CAPTIVE_PORTAL_REDIRECT_FS_KEY       "cpportal"
+#define CAPTIVE_PORTAL_PREF_FS_KEY           "cpprefs"
+#define CAPTIVE_PORTAL_CHANNEL               "cpchan"
+#define CAPTIVE_PORTAL_NET_NAME              "WIFI_AP_DEF"
 
 // USER INputs
 #define CAPTIVE_USER_INPUT1 "user1"
