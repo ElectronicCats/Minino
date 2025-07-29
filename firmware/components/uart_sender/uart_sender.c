b6a78659afe84f052315ca3f32ab6a4e31fb7f76
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2025-07-29 16:07:04 -0600

    feat: Adv scanner add

--- firmware/components/uart_sender/uart_sender.c
+++ firmware/components/uart_sender/uart_sender.c
@@ -2,25 +2,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include "esp_log.h"
 
-#define BLE_ADDRESS_SIZE    6
+#define BLE_ADDRESS_SIZE    4
 #define BLE_PDU_INFO_OFFSET 19
 #define BLE_ADDRESS_OFFSET  21
 #define BLE_PAYLOAD_OFFSET  27
+#define ESP_BLE_ADV_MAX_LEN 31
 
 static bool is_uart_sender_initialized = false;
-
-static unsigned char base_packet[] = {
-    0x40, 0x53,                          // Start - TI packet
-    0xc0,                                // Packet Info - TI Packet
-    0x36, 0x00,                          // Packet Length - TI Packet
-    0x8c, 0xc9, 0xaf, 0x01, 0x00, 0x00,  // Timestamp - TI Packet
-    0x25,                                // Channel - TI Packet
-    0x00, 0x00,                          // Connection Event - TI Packet
-    0x00,                                // Connection Info - TI Packet
-    0xd6, 0xbe, 0x89, 0x8e,              // Access Address - TI Packet
-};
 
 /**
  * @brief Initialize the UART sender
@@ -72,20 +63,6 @@ void uart_sender_send_packet(uart_sender_packet_type type,
   printf("\n");
 }
 
-// unsigned char array[] = {
-
-//     // Header - BLE Packet
-//     0x42, // PDU Info - BLE Packet
-//     0x21, // AdvLen - BLE Packet
-//     0x15, 0xc7, 0xbf, 0x84, 0x50, 0x4b, // AdvA - BLE Packet
-//     // Payload - BLE Packet
-//     0x02, 0x01, 0x1a, 0x17, 0xff,
-//     0x4c, 0x00, 0x09, 0x08, 0x13, 0x03, 0xc0, 0xa8, 0x00, 0xb2, 0x1b, 0x58,
-//     0x16, 0x08, 0x00, 0xa6, 0x48, 0x4c, 0x4f, 0x71, 0xdf, 0xb7, 0xf1, 0x9b,
-//     0xee, 0xc7, // RSSI - TI Packet 0x80, // Status - TI Packet 0x40, 0x45 //
-//     END - TI packet
-// };
-
 uint32_t calculate_ble_crc24(const uint8_t* data, size_t len) {
   uint32_t crc = 0x555555;  // Inicialización del CRC con 0x555555
   uint32_t poly = 0x65B;    // Polinomio 0x65B
@@ -105,44 +82,28 @@ uint32_t calculate_ble_crc24(const uint8_t* data, size_t len) {
   return (crc & 0xFFFFFF);  // Retornar solo los 24 bits inferiores
 }
 
-void uart_sender_send_packet_ble(uart_sender_packet_type type,
-                                 esp_ble_gap_cb_param_t* packet) {
-  uart_sender_init();
-  uint8_t packet_length =
-      1 + 1 + BLE_ADDRESS_SIZE + packet->scan_rst.adv_data_len;
-  for (int i = 0; i < sizeof(base_packet); i++) {
-    printf("%c", base_packet[i]);
-  }
-  unsigned char temp_packet[packet_length];
-  uint8_t index = 0;
-  temp_packet[index] = packet->scan_rst.ble_evt_type;
-  index++;
-  temp_packet[index] = packet->scan_rst.adv_data_len;
-  index++;
-  for (int i = 0; i < BLE_ADDRESS_SIZE; i++) {
-    temp_packet[index] = packet->scan_rst.bda[i];
-    index++;
-  }
-  for (int i = 0; i < packet->scan_rst.adv_data_len; i++) {
-    temp_packet[index] = packet->scan_rst.ble_adv[i];
-    index++;
-  }
-  // printf("%c", packet->scan_rst.ble_evt_type);
-  // printf("%c", packet->scan_rst.adv_data_len);
-  // for (int i = 0; i < BLE_ADDRESS_SIZE; i++) {
-  //   printf("%c", packet->scan_rst.bda[i]);
-  // }
-  // for (int i = 0; i < packet->scan_rst.adv_data_len; i++) {
-  //   printf("%c", packet->scan_rst.ble_adv[i]);
-  // }
-  for (int i = 0; i < packet_length; i++) {
-    printf("%c", temp_packet[i]);
-  }
+bool is_adv_structure_valid(const uint8_t* data, uint8_t len) {
+  if (len < 2)
+    return false;
+  uint8_t type = data[1];
 
-  uint16_t crc = calculate_ble_crc24(temp_packet, packet_length);
-  printf("%02x%02x", (crc >> 8) & 0xFF, crc & 0xFF);
-  printf("%c", packet->scan_rst.rssi);
-  printf("\x80");  // Status
-  printf("@E");    // End of packet
-  printf("\n");
+  switch (type) {
+    case 0x0C:
+      if (len < 4)
+        return false;
+      break;
+    case 0xFF:
+      if (len < 3)
+        return false;
+      break;
+    case 0x01:
+      if (len < 3)
+        return false;
+      break;
+    default:
+      if (len < 2)
+        return false;
+      break;
+  }
+  return true;
 }
