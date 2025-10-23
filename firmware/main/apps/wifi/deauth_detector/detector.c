45576f4282a4aae5631f16e46899fccea734167e
AdonaiDiaz <carlosadonai.de0208@gmail.com>
2025-10-23 12:11:01 -0600

    feat(fw): delete unused variable

--- firmware/main/apps/wifi/deauth_detector/detector.c
+++ firmware/main/apps/wifi/deauth_detector/detector.c
@@ -29,7 +29,7 @@ static void packet_handler(void* buf, wifi_promiscuous_pkt_type_t type) {
   wifi_promiscuous_pkt_t* p = (wifi_promiscuous_pkt_t*) buf;
   wifi_pkt_rx_ctrl_t rx_ctrl = p->rx_ctrl;
   uint8_t* mac = p->payload;
-  uint8_t buf_alternative = buf[12];
+
   if (pkt_type == 0xA0 || pkt_type == 0xC0) {
     ESP_LOGI(TAG,
              "Packet received channel: %d from MAC address: "
