be8d000a44903444dad8376066a750ccbf8f2d83
AdonaiDiaz <carlosadonai.de0208@gmail.com>
2025-10-29 17:06:31 -0600

    test(fw): add a test project

---
+++ firmware_refactor/main/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+static const char* TAG = "main";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Starting firmware refactor");
+
+    while (1)
+    {
+        
+    }
+    
+}
