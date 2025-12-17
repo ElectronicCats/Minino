e3bc202c8b3d22cf7a4dd1726dab2d68c4e9f8f2
Donuts Diaz <carlosadonai.de0208@gmail.com>
2025-12-17 15:51:05 -0600

    refactor(fw): move zigbee and thread files

--- firmware/deprecated_components/deprecated_apps/thread_sniffer/thread_sniffer_screens.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "thread_sniffer.h"
-
-void thread_sniffer_show_event_handler(thread_sniffer_events_t event,
-                                       void* context);
--- firmware/deprecated_components/deprecated_modules/open_thread/open_thread_screens_module.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void open_thread_screens_display_broadcast_mode();
-
-void open_thread_screens_show_new_message(void* msg);
