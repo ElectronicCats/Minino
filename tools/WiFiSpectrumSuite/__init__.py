a9de6d988c7e47bf47016d6dcd19443e87dc5ca6
Darcko123 <daniel18052002@yahoo.com>
2026-03-17 15:27:58 -0600

    feat: add initial files for WiFi Spectrum Suite including core, utils, and visualization modules

---
+++ tools/WiFiSpectrumSuite/__init__.py
@@ -0,0 +1,7 @@
+"""
+WiFi Spectrum Suite - Complete Suite for WiFi Analysis
+Integrated Tools for WiFi Analysis: CSV Debugger, Interference Analysis, and Wardriving Analyzer
+"""
+ 
+__version__ = "1.1.0.0"
+__author__ = "Electronic Cats  & Dr. h. c.* César A. Peregrino Rodríguez"
